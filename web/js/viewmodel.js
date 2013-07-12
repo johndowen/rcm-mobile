0aebaff7b7cfd225912c9d1fba31c03df1180c03
SimonKeep <Simon_Keep@hotmail.com>
2013-07-12 16:34:44 +0100

    Wire up to search service

---
+++ web/js/viewmodel.js
@@ -0,0 +1,30 @@
+﻿function AppViewModel() {
+
+    this.searchCriteria = ko.observable("bill");
+    this.results = ko.observable();
+    this.isValid = function () {
+        return this.searchCriteria().length > 0;
+    };
+
+    this.search = function () {
+
+        var script = $("<script src='https://rcmdemo.boardex.com/dataservices/IndividualService.svc/jsonp/names/?apiKey=32449927-4cc5-41ad-ba82-bd72510591ac&criteria=" + this.searchCriteria() + "&format=json&successCallback=x.searchResponse'></script>");
+        $("head").append(script);
+    };
+
+
+    this.searchResponse = function(responseString) {
+        this.results($.parseJSON(responseString));
+        alert(this.results().Count);
+    };
+
+    
+}
+
+
+
+
+
+// Activates knockout.js
+var x = new AppViewModel();
+ko.applyBindings(x);
