e6bb3f990ca03be669d7cbb7a5888e2df97bbe7e
SimonKeep <Simon_Keep@hotmail.com>
2013-07-12 16:58:29 +0100

    Show error

--- web/js/viewmodel.js
+++ web/js/viewmodel.js
@@ -2,23 +2,28 @@
 
     this.searchCriteria = ko.observable("bill");
     this.results = ko.observable();
+    this.errorx = ko.observable();
     this.isValid = function () {
         return this.searchCriteria().length > 0;
     };
 
     this.search = function () {
 
-        var script = $("<script src='https://rcmdemo.boardex.com/dataservices/IndividualService.svc/jsonp/names/?apiKey=32449927-4cc5-41ad-ba82-bd72510591ac&criteria=" + this.searchCriteria() + "&format=json&successCallback=x.searchResponse'></script>");
+        var script = $("<script src='https://rcmdemo.boardex.com/dataservices/IndividualService.svc/jsonp/names/?apiKey=32449927-4cc5-41ad-ba82-bd72510591ac&criteria=" + this.searchCriteria() + "&format=json&successCallback=x.searchResponse&errorCallback=x.errorc'></script>");
         $("head").append(script);
     };
 
 
     this.searchResponse = function(responseString) {
         this.results($.parseJSON(responseString));
-        alert(this.results().Count);
     };
 
-    
+    this.errorc = function (responseString) {
+        this.errorx($.parseJSON(responseString));
+
+    };
+
+
 }
 
 
